d3bf150e95de6b462b29e1dd238d77566c700214
Rashi agrawal <117668734+rashi12367@users.noreply.github.com>
2023-11-25 19:05:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,105 +1,134 @@
-
 const express = require('express');
-require('./Dbconnect');
-const hbs=require('hbs')
-const Park = require('./models/park');
-const bodyParser=require('body-parser');   //may remove
-const app = express();
+const calculateFare = require('./calculateFare'); // Import the calculateFare function
+const hbs = require('hbs');
+const Park = require('./models/park'); // Import the Park model
 const path = require('path');
-const encoder=bodyParser.urlencoded()
-app.set('views', './views'); // Set the views directory to './views'
-app.set("view engine","hbs")
-const partialPath=path.join(__dirname,"./views/partials")
-hbs.registerPartials(partialPath)
-const staticPath=path.join(__dirname,"./views/public")
-app.use(express.static(staticPath))
+const bodyParser = require('body-parser'); // For form data parsing
+const moment = require('moment'); // Import Moment.js for date and time calculations
+require('./Dbconnect'); // Connect to MongoDB database
 
-// const mongoose = require('mongoose');
-// const path=require('path');                 //may remove
+const app = express();
+const encoder = bodyParser.urlencoded(); // Middleware for parsing URL-encoded form data
 
+// Set view engine and views directory
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
-// Body parser middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+// Register partials directory for Handlebars templates
+const partialPath = path.join(__dirname, './views/partials');
+hbs.registerPartials(partialPath);
 
+// Serve static files from the public directory
+const staticPath = path.join(__dirname, './views/public');
+app.use(express.static(staticPath));
 
-// Get parked vehicles
+// Body parser middleware for parsing JSON and URL-encoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Get all parked vehicles
 app.get('/', async (req, res) => {
-    try {
-      const parkedVehicles = await Park.find();
-      res.render('index', { data: parkedVehicles });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error retrieving parked vehicles');
-    }
+  const data = await Park.find(); // Fetch all parked vehicles from the database
+  res.render('index', { data }); // Render the index view with the parked vehicles data
+});
+
+// Add a new parked vehicle
+app.get('/add', (req, res) => {
+  res.render('add'); // Render the add form view
+});
+
+app.post('/add', async (req, res) => {
+  // Extract and validate input data
+  const Car_Registration_number = req.body.Car_Registration_number?.trim() || '';
+  const Owner = req.body.Owner.trim() || '';
+  const arrivalTimeStr = req.body.Arrival;
+  const departureTimeStr = req.body.Departure;
+
+  // Format arrivalTime and departureTime using moment.js
+  const arrivalTime = moment(arrivalTimeStr, 'HH:mm');
+  const departureTime = moment(departureTimeStr, 'HH:mm');
+
+  // Check for missing fields
+//   if (!Car_Registration_number || !Owner || !arrivalTime || !departureTime) {
+//     return res.status(400).send('Missing required fields: Car Registration number, Owner, Arrival, Departure');
+//   }
+
+  // Calculate fare using the calculateFare function
+  const fare = calculateFare(arrivalTimeStr, departureTimeStr);
+
+  // Create and save a new Park object
+  const newPark = new Park({
+    Car_Registration_number,
+    Owner,
+    Arrival: arrivalTime.format('HH:mm'), // Format arrivalTime before saving
+    Departure: departureTime.format('HH:mm'), // Format departureTime before saving
+    Fare: fare,
   });
-  
-  app.get('/add', (req, res) => {
-    res.render('add');
-  });
-  
-  app.post('/add', async (req, res) => {
-    try {
-        console.log(req.body);
-       const newPark = new Park({
-        Car_Registration_number: "1234",
-        Owner: "Govind",
-        Arrival: "23-12-3212 15:21",
-        Departure: "24-12-3212 15:21",
-        // vehicleType: req.body.vehicleType
-      });
-  
-      await newPark.save();
-      res.redirect('/');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error adding parking slot');
-    }
-  });
-  
-  app.get('/deleteSlot/:id', async (req, res) => {
-    try {
-      await Park.deleteOne({ _id: req.params.id });
-      res.redirect('/');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error deleting parking slot');
-    }
-  });
-  
-  app.get('/updateSlot/:id', async (req, res) => {
-    try {
-      const parkToUpdate = await Park.findById(req.params.id);
-      if (!parkToUpdate) {
-        return res.status(404).send('Parking slot not found');
+
+  await newPark.save(); // Save the new parked vehicle to the database
+  res.redirect('/'); // Redirect to the home page
+});
+
+// Get fare calculation
+app.get('/fareCalculation', async (req, res) => {
+  const arrivalTime = req.query.Arrival;
+  const departureTime = req.query.Departure;
+
+  // Calculate fare using the calculateFare function
+  const fare = calculateFare(arrivalTime, departureTime);
+  res.send({ fare }); // Send the calculated fare as JSON
+});
+
+// Get available parking slots
+app.get('/availableSlots', async (req, res) => {
+  const totalSlots = parseInt(req.query.totalSlots); // Get the total number of parking slots
+  const parkedVehicles = await Park.find(); // Fetch all parked vehicles from the database
+
+  const availableSlots = []; // Initialize an empty array to store available slots
+
+  for (let i = 1; i <= totalSlots; i++) {
+    let slotAvailable = true; // Assume the slot is initially available
+
+    // Check if the slot is occupied by any parked vehicle
+    for (const parkedVehicle of parkedVehicles) {
+      if (parkedVehicle.slotNumber === i) {
+        slotAvailable = false; // Mark the slot as occupied
+        break;
       }
-  
-      res.render('update', { data: parkToUpdate });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error retrieving parking slot');
     }
+
+    // Add the available slot to the array if it's not occupied
+    if (slotAvailable) {
+      availableSlots.push(i);
+    }
+  }
+
+  res.send({
+    availableSlots,
+  });
+});
+// Delete parked vehicle
+app.get('/deleteSlot/:id', async (req, res) => {
+    await Park.deleteOne({ _id: req.params._id });
+    res.redirect('/');
+  });
+  
+  // Update parked vehicle
+  app.get('/updateSlot/:id', async (req, res) => {
+    const data = await Park.findOne({ _id: req.params._id });
+    res.render('update', { data });
   });
   
   app.post('/updateSlot/:id', async (req, res) => {
-    try {
-      const updatedPark = await Park.findByIdAndUpdate(req.params.id, {
-        carRegistrationNumber: req.body.carRegistrationNumber,
-        owner: req.body.owner,
-        arrivalTime: req.body.arrivalTime,
-        departureTime: req.body.departureTime,
-        // vehicleType: req.body.vehicleType
-      });
+    const data = await Park.findOne({ _id: req.params._id });
+    data.Car_registration_number = req.body.Car_registration_number;
+    data.Owner = req.body.Owner;
+    data.Arrival = req.body.Arrival;
+    data.Departure = req.body.Departure;
   
-      if (!updatedPark) {
-        return res.status(404).send('Parking slot not found');
-      }
-  
-      res.redirect('/');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error updating parking slot');
-    }
+    await data.save();
+    res.redirect('/');
   });
   
-  app.listen(3050, () => console.log('Server started on port 3050'));
+  // Server listen
+  app.listen(3050, () => console.log('Server listening to port 3050'));
